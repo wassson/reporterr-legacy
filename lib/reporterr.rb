0cf6e55c9464292604d040433dc1d4a61f47b5a6
Austin Wasson <austinpwasson@gmail.com>
2024-01-08 18:22:46 -0700

    reporterr

---
+++ lib/reporterr.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "reporterr/version"
+
+module Reporterr
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/reporterr/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module Reporterr
+  VERSION = "0.1.0"
+end
