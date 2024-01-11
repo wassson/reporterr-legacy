cfe507aa4717bc97d1078240a94f226dc3a53acd
Austin Wasson <austinpwasson@gmail.com>
2024-01-11 10:31:17 -0700

    update context builder (#1)

--- test/dummy/app/controllers/marketing_controller.rb
+++ test/dummy/app/controllers/marketing_controller.rb
@@ -1,11 +1,19 @@
 # frozen_string_literal: true
 
 class MarketingController < ApplicationController
+  # before_action :break_controller
+
   def index
-    # Rails.error.handle(StandardError, severity: :info, context: { user_id: 3 } ) do
-    # Rails.error.handle(StandardError) do
-    #   puts 'Hello!'
-    #   raise StandardError
-    # end
+    Rails.error.handle(StandardError, context: { ip_address: '192.168.1.1' }) do
+      raise StandardError
+    end
+  end
+
+  private
+
+  def break_controller
+    Rails.error.handle(StandardError) do
+      raise StandardError
+    end
   end
 end
